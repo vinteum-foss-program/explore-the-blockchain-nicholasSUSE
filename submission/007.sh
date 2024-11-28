1b6d58af2a2fd5233ec5acc76acb1d59da56efe3
nicholasSSUSE <nicholas.paolillo@suse.com>
2024-11-27 21:53:21 -0300

    7th challenge

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+
+blockhash=$(bitcoin-cli getblockhash 123321)
+block=$(bitcoin-cli getblock $blockhash)
+tx_ids=$(echo $block | jq -r '.tx[]')
+
+for tx_id in $tx_ids; do
+  raw_tx=$(bitcoin-cli getrawtransaction $tx_id 1)
+
+  if [ -z "$raw_tx" ]; then
+    continue
+  fi
+
+  vout=$(echo $raw_tx | jq -c '.vout')
+
+  if [ -z "$vout" ] || [ "$vout" = "[]" ];then
+    continue
+  fi
+
+  echo "$vout" | jq -c '.[]' | while read -r item; do
+    n=$(echo "$item" | jq '.n')
+    utxo=$(bitcoin-cli gettxout $tx_id $n true)
+    if [ -z "$utxo" ]; then
+        continue
+    fi
+    address=$(echo "$utxo" | jq -r '.scriptPubKey.address')
+    echo $address
+  done
+
+done
